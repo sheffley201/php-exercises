a5734280e2c54af6586f7368ad098eb1a90e8b7e
Spencer Heffley <spheffley@yahoo.com>
2021-10-08 09:25:15 -0400

    Added files for second challenge and started the HTML

---
+++ lecture5challenges/set.php
@@ -0,0 +1,3 @@
+<?php
+    require 'set.view.php';
+?>
---
+++ lecture5challenges/set.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--jQuery CDN-->
+        <script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous"></script>
+
+        <!--Bootstrap CDNs-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <title>Set Calculator</title>
+    </head>
+    <body>
+        <div class="container-fluid bg-primary p-3">
+            <h1 class='display-1 text-center text-white'>Set Calculator</h1>
+        </div>
+    </body>
+</html>
